5f5fff3f450a656b264b46b24e749c8066916c59
rayissach <rayissach@gmail.com>
2018-03-04 01:47:44 -0800

    working html routes

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
