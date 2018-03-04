65af7e270880aa993aa0dec3f0daf0ceae1b746b
rayissach <rayissach@gmail.com>
2018-03-04 11:27:09 -0800

    added css styling and api routes

---
+++ routing/apiRoutes.js
@@ -0,0 +1,37 @@
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
+
+	app.get("/api/friends", function(req, res){
+		res.json(friends)
+	});
+
+	app.get("/api/data",function(req, res) {
+		res.json(friends)
+	});
+
+	app.post("api/friends", function(req, res) {
+		
+		var results = req.body;
+
+		results.name = results.name.replace(/\s+/g, "");
+		results.photo = results.photo.replace(/\s+/g, "");
+
+		var compName = "";
+		var compPhoto = "";
+		var newScore = results.scores;
+
+		var difference = 10000;
+
+		for (var i = 0; friends.length; i++) {
+			var initDiff = 0;
+		}
+
+	var result = [friends[0]];
+    res.json({
+      status: 'OK',
+      result
+    });
+	})
+};
+
